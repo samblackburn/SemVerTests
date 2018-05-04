86739d1d2c3eba85bc24638b37ff3896f763119b
Sam Blackburn <sam.blackburn@red-gate.com>
2018-05-04 15:46:09 +0100

    Green: References

--- ProgrammaticCompilationTests.cs
+++ ProgrammaticCompilationTests.cs
@@ -17,5 +17,22 @@ namespace SemVerTests
             dynamic foo = assembly.CreateInstance("Foo");
             Assert.AreEqual(1, foo.Version());
         }
+
+        [Test]
+        public void CanCompileADllAgainstADependency()
+        {
+            var compiler = new CSharpCodeProvider();
+
+            var cs1 = @"public class Foo{public int Version() {return 1;}}";
+            var assembly1 = compiler
+                .CompileAssemblyFromSource(new CompilerParameters(new string[0], "foo.dll"), cs1)
+                .CompiledAssembly;
+            var cs2 = @"public class Bar{public int AccessFoo() {return new Foo().Version();}}";
+            var assembly2 = compiler
+                .CompileAssemblyFromSource(new CompilerParameters(new[] {"foo.dll"}, "bar.dll"), cs2);
+            CollectionAssert.IsEmpty(assembly2.Errors);
+            dynamic bar = assembly2.CompiledAssembly.CreateInstance("Bar");
+            Assert.AreEqual(1, bar.AccessFoo());
+        }
     }
 }
