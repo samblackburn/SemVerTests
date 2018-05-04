889ac4fbae08e5eb58531b863027e44ad24d8390
Sam Blackburn <sam.blackburn@red-gate.com>
2018-05-04 15:38:10 +0100

    Green: Compile and run some code to simulate loading a plugin

---
+++ ProgrammaticCompilationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using NUnit.Framework;
+
+namespace SemVerTests
+{
+    public class ProgrammaticCompilationTests
+    {
+        [Test]
+        public void CanCompileAndLoadADll()
+        {
+            var cs = @"public class Foo{public int Version() {return 1;}}";
+            var assembly = new CSharpCodeProvider()
+                .CompileAssemblyFromSource(new CompilerParameters(), cs)
+                .CompiledAssembly;
+            dynamic foo = assembly.CreateInstance("Foo");
+            Assert.AreEqual(1, foo.Version());
+        }
+    }
+}
