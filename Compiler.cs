dda02e2bf6c037418790c74d42d36af934e7818e
Sam Blackburn <sam.blackburn@red-gate.com>
2018-05-04 17:58:32 +0100

    A dll that doesn't recognise the dll it was compiled against

--- Compiler.cs
+++ Compiler.cs
@@ -8,14 +8,25 @@ namespace SemVerTests
     internal class Compiler
     {
         private const string c_MsBuild = @"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe";
+        private static int s_AssemblyNumber = 0;
 
-        internal static string CompileAndCopyLocal(string cs)
+        internal static string CompileAndCopyLocal(string cs, string reference = null)
         {
-            var assemblyName = Path.GetRandomFileName();
+            var assemblyName = "Assembly" + ++s_AssemblyNumber;
             using (var tempDir = new TempDir())
             {
+                if (reference != null)
+                {
+                    var copyOfReferenceDll = tempDir.PathTo(Path.GetFileName(reference));
+                    File.Copy(reference, copyOfReferenceDll);
+                    tempDir[assemblyName + ".csproj"] = CsprojWithReference("class.cs", copyOfReferenceDll);
+                }
+                else
+                {
+                    tempDir[assemblyName + ".csproj"] = Csproj("class.cs");
+                }
+
                 tempDir["class.cs"] = cs;
-                tempDir[assemblyName + ".csproj"] = Csproj("class.cs");
                 RunMsbuild(tempDir, assemblyName + ".csproj");
 
                 var dll = tempDir.PathTo($@"bin\Debug\{assemblyName}.dll");
@@ -43,6 +54,24 @@ namespace SemVerTests
   </PropertyGroup>
   <ItemGroup>
     <Compile Include=""{sourceFile}"" />
+  </ItemGroup>
+  <Import Project=""$(MSBuildToolsPath)\Microsoft.CSharp.targets"" />
+</Project>";
+        }
+
+        private static string CsprojWithReference(string sourceFile, string referenceDll)
+        {
+            return $@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project ToolsVersion=""15.0"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <PropertyGroup>
+    <OutputType>Library</OutputType>
+    <TargetFrameworkVersion>v4.0</TargetFrameworkVersion>
+  </PropertyGroup>
+  <ItemGroup>
+    <Compile Include=""{sourceFile}"" />
+     <Reference Include=""{Path.GetFileNameWithoutExtension(referenceDll)}"">
+      <HintPath>{referenceDll}</HintPath>
+    </Reference>
   </ItemGroup>
   <Import Project=""$(MSBuildToolsPath)\Microsoft.CSharp.targets"" />
 </Project>";
--- MsBuildTests.cs
+++ MsBuildTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.IO;
+using System.Reflection;
 using NUnit.Framework;
 
 namespace SemVerTests
@@ -11,6 +12,21 @@ namespace SemVerTests
             var assembly = Compiler.CompileAndCopyLocal(@"public class Foo{public int Version() {return 1;}}");
 
             Assert.AreEqual(1, ((dynamic) Assembly.LoadFile(assembly).CreateInstance("Foo")).Version());
+        }
+
+        [Test]
+        public void ReferenceADll()
+        {
+            var assembly1 = Compiler.CompileAndCopyLocal(@"public class Foo{public int Version() {return 1;}}");
+            var assembly2 = Compiler.CompileAndCopyLocal(@"public class Bar{public int AccessFoo() {return new Foo().Version();}}", assembly1);
+
+            dynamic foo = Assembly.LoadFile(assembly1).CreateInstance("Foo");
+            dynamic bar = Assembly.LoadFile(assembly2).CreateInstance("Bar");
+
+            Assert.AreEqual(1, foo.Version());
+            var ex = Assert.Throws<FileNotFoundException>(() => bar.AccessFoo());
+
+            StringAssert.Contains(Path.GetFileNameWithoutExtension(assembly1), ex.Message);
         }
     }
 }
