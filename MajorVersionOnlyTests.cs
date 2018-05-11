d6d491ddb239b31684153e9a2cfd98f22ba99d48
Sam Blackburn <sam.blackburn@red-gate.com>
2018-05-11 16:46:31 +0100

    Refine the test to just require a new method to be added

--- MajorVersionOnlyTests.cs
+++ MajorVersionOnlyTests.cs
@@ -8,23 +8,26 @@ namespace SemVerTests
         [Test]
         public void ApiChangesWithoutMajorVersionBumpCauseCrashesWhenSeveralProductsAreInstalled()
         {
-            var c_SharedLibrary_source_old = @"public class Utils { public void Deprecated() {} }";
-            var c_SharedLibrary_source_new = @"public class Utils {}";
-            var c_Ui_source = @"public class Ui { public void Show() { new Utils().Deprecated(); } }";
+            var c_SharedLibrary_source_old = @"public class Utils { }";
+            var c_SharedLibrary_source_new = @"public class Utils { public void NewMethod() {} }";
+            var c_Prompt_source = @"public class Ui { public void Show() { new Utils().NewMethod(); } }";
+            var c_Soc_source = "public class Ui { public void Show() { } }";
 
             var compiler = new Compiler();
 
-            // Compile the engine against the shared library
+            // Compile SOC against shared library version 18.1
             var oldSharedLibrary = compiler.CompileAndCopyLocal("SharedLibrary", c_SharedLibrary_source_old, version: new Version(18, 0, 0, 0)).Rename("SharedLib_old.dll");
-            var soc = compiler.CompileAndCopyLocal("Soc", c_Ui_source, oldSharedLibrary);
+            var soc = compiler.CompileAndCopyLocal("Soc", c_Soc_source, oldSharedLibrary);
+
+            // Compile Prompt against shared library version 18.2
             var newSharedLibrary = compiler.CompileAndCopyLocal("SharedLibrary", c_SharedLibrary_source_new, version: new Version(18, 0, 0, 0)).Rename("SharedLib_new.dll");
-            var prompt = compiler.CompileAndCopyLocal("Prompt", "", newSharedLibrary);
+            var prompt = compiler.CompileAndCopyLocal("Prompt", c_Prompt_source, newSharedLibrary);
             
-            // SSMS comes along and loads everything
+            // SSMS comes along and loads everything, unfortunately in the wrong order
+            oldSharedLibrary.CreateInstance("Utils");
             newSharedLibrary.CreateInstance("Utils");
-            oldSharedLibrary.CreateInstance("Engine");
-            prompt.CreateInstance("UI");
-            var ui = soc.CreateInstance("Ui");
+            soc.CreateInstance("UI");
+            var ui = prompt.CreateInstance("Ui");
 
             // But sadly...
             var ex = Assert.Throws<MissingMethodException>(() => ui.Show());
